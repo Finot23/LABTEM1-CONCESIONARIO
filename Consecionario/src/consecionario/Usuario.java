9344f09bbaebcdde1754812bda92cc804292201d
Finot23 <luis.ridr78@gmail.com>
2025-03-30 10:49:38 -0600

    Se agregaron la clase de Usuarios el cual contiene id como variable de tipo entero, nombre como variable de tipo String, contraseña como variable de tipo String, Rol como variable de tipo String. A todas estas variables se les agrego sus espectivos constructores, getters y setters.

---
+++ Consecionario/src/consecionario/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package consecionario;
+
+/**
+ *
+ * @author antoniosalinas
+ */
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String contaseña;
+    int rol;
+
+    public Usuario(int id, String nombre, String contaseña, int rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contaseña = contaseña;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContaseña() {
+        return contaseña;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setContaseña(String contaseña) {
+        this.contaseña = contaseña;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+ 
+    
+}
