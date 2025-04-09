4741a6fe3d54fa08f3d0d999495b61b6b374bb48
Finot <Finot@DESKTOP-9L0HOOI>
2025-04-09 12:17:21 -0600

    Se agrego la clase de RedimencionarImagen la cual permitira que cuando se requiera ajustar una imagen al tamano del label solo se llame a la clase se implemente el metodo. Tambien se agrego el Form de categoria el cual servira para elegir el tipo de coche que requiere el cliente

---
+++ Consecionario/src/consecionario/RedimencionarImagen.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package consecionario;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Finot
+ */
+public class RedimencionarImagen {
+     public void setImageToLabel(JLabel label, String imageResourcePath) {
+        // 1. Cargar imagen desde los recursos del proyecto (dentro de src/)
+        ImageIcon originalIcon = new ImageIcon(getClass().getResource(imageResourcePath));
+
+        // 2. Redimensionar al tamaño actual del JLabel
+        Image scaledImage = originalIcon.getImage().getScaledInstance(
+            label.getWidth(),
+            label.getHeight(),
+            Image.SCALE_SMOOTH
+        );
+
+        // 3. Asignar imagen escalada al JLabel
+        label.setIcon(new ImageIcon(scaledImage));
+}
+}
